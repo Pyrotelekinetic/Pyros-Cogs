4447626b069bb6216561960981fa74cb63401401
Pyrotelekinetic <Pyrotelekinetic@users.noreply.github.com>
2017-07-03 22:04:59 -0700

    removed mock command

--- autoquote.py
+++ autoquote.py
@@ -17,43 +17,6 @@ class autoquote:
 			search = self.bot.all_log[message.channel.id + " " + message.server.id][-2]
 			result = search[0]
 			await self.bot.send_message(message.channel, "{}quote {}".format(self.cmd_prefix[0], result.id))
-
-			
-			
-	@commands.command(pass_context=True)
-	async def mock(self, ctx, *, msg=""):
-		"""Use >mock to randomized capitalization on a message or string.
-		More random comming soon™
-		Usage:
-		>mock a string
-			A sTRiNg
-		>mock
-			laSt SeNT MeSsaGE
-		>mock (message id)
-			THaT mEsSAgE
-		"""
-		result = ""
-		if msg:
-			if msg.isdigit():
-				async for message in self.bot.logs_from(ctx.message.channel, 100):
-					if message.id == msg:
-						msg = message.content
-						break
-		else:
-			switch = False
-			async for message in self.bot.logs_from(ctx.message.channel, 2):
-				if switch:
-					msg = message.content
-				else:
-					switch = True
-		for char in msg:
-			value = random.choice([True, False])
-			if value == True:
-				result += char.upper()
-			if value == False:
-				result += char.lower()
-		await self.bot.delete_message(ctx.message)
-		await self.bot.send_message(ctx.message.channel, result)
 		
 def setup(bot):
     bot.add_cog(autoquote(bot))
